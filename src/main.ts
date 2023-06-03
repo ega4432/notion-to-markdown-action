6c4376771015c5cc269a2cd2193b133a78553614
ega4432 <38056766+ega4432@users.noreply.github.com>
2023-06-03 16:37:20 +0900

    chore: fixed excutable ts-node

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,16 @@ import { Client } from '@notionhq/client';
 import { writeFile } from 'fs/promises';
 import get from 'axios';
 
-import { queryDatabase } from './utils/notion';
+import { queryDatabase } from './utils/notion.js';
 import {
   convertPagesToMarkdown,
   findImagesFromMarkdown,
   MarkdownPage
-} from './utils/markdown';
+} from './utils/markdown.js';
 import {
   DEFAULT_FILENAME_PROPERTY,
   DEFAULT_OUTPUT_DIR
-} from './utils/constants';
+} from './utils/constants.js';
 
 const auth = process.env.NOTION_API_KEY as string;
 const databaseId = process.env.NOTION_DATABASE_ID as string;
